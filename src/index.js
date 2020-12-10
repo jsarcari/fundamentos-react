4e2b7d49e2054864e3335669b124adcad0fa5b98
jsarcari <juansantos1995@hotmail.com>
2020-12-10 19:13:37 -0300

    Criaçao dos componentes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+
 ReactDOM.render(
-    <h1>Olá React!</h1>,
+    <>
+        <Primeiro />
+        <ComParametro titulo="Aqui está o título" subtitulo="teste"></ComParametro>
+    </>,
     document.getElementById('root')
 )
