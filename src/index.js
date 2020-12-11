40cdeb8bb2c66ae9f5c2323d427964227deaf926
jsarcari <juansantos1995@hotmail.com>
2020-12-10 23:52:53 -0300

    Criaçao dos componentes de layout

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom'
 
 import Primeiro from './components/Primeiro'
 import ComParametro from './components/ComParametro'
+import Card from './components/layout/Card'
 
 ReactDOM.render(
     <>
-        <Primeiro />
+        <Card titulo="Card">
+            Conteudo
+        </Card>
+        <Card titulo="Componente">
+            <Primeiro />
+        </Card>
         <ComParametro titulo="Aqui está o título" subtitulo="teste"></ComParametro>
     </>,
     document.getElementById('root')
